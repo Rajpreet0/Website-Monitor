403395b0eba649458d1811578ef296d00de3ff0b
Rajpreet0 <rajpreet.singh@gmx.de>
2024-06-15 21:55:48 +0200

    added WebsiteMonitor Class

---
+++ src/WebsiteMonitor.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class WebsiteMonitor {
+
+    private List<WebsiteSubscription> subscriptions;
+
+
+    public void addSubscription(WebsiteSubscription subscription) {
+
+    }
+
+    public void removeSubscription(String subscriptionId) {
+
+    }
+
+    public void checkForUpdate() {
+
+    }
+
+    public void notifyUsers(String websiteUrl, String updateMessage) {
+
+    }
+
+}
